f4b7ef035a35d1e0c46357d8067926068f45b50d
starr K <jimin326ww@gmail.com>
2025-10-19 21:28:55 +0900

    1019

---
+++ src/pages/Docent.js
@@ -0,0 +1,20 @@
+// src/pages/Docent.js
+import React, { useState, useEffect, useRef } from "react";
+import "../styles/Global.css";
+import "../styles/Docent.css";
+
+import NavbarBottom from "../components/NavbarBottom"; // ✅ 추가
+
+
+function Docent() {
+
+  return ( 
+    <div>
+
+      <NavbarBottom />
+
+    </div>
+  );
+}
+
+export default Docent;
